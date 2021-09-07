8dcd14616bf88e5468df7bf626078829567653b7
Olle Hallin <olle.hallin@hit.se>
2021-09-07 08:44:57 +0200

    Added `awsprofile` as alias to `aws_token`.

--- ansible/files/fish/functions/aws_token.fish
+++ ansible/files/fish/functions/aws_token.fish
@@ -6,3 +6,12 @@ function aws_token
   set -gx AWS_PROFILE "$aws_profile"
   aws --no-cli-pager sts get-caller-identity
 end
+
+function awsprofile
+  set aws_profile (aws configure list-profiles | fzf --header 'AWS profile')
+  if ! aws --profile "$aws_profile" sts get-caller-identity > /dev/null 2>&1
+      gimme-aws-creds --profile "$aws_profile"
+  end
+  set -gx AWS_PROFILE "$aws_profile"
+  aws --no-cli-pager sts get-caller-identity
+end
