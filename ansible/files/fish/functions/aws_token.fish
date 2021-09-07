dc95fe805e1066a26758b9d7e97998c525aa327c
Olle Hallin <olle.hallin@hit.se>
2021-09-07 08:54:16 +0200

    Corrected bug in aws_token.fish

--- ansible/files/fish/functions/aws_token.fish
+++ ansible/files/fish/functions/aws_token.fish
@@ -1,17 +1,21 @@
 function aws_token
   set aws_profile (aws configure list-profiles | fzf --header 'AWS profile')
-  if ! aws --profile "$aws_profile" sts get-caller-identity > /dev/null 2>&1
-      gimme-aws-creds --profile "$aws_profile"
+  if test -n "$aws_profile"
+      if ! aws --profile "$aws_profile" sts get-caller-identity > /dev/null 2>&1
+          gimme-aws-creds --profile "$aws_profile"
+      end
+      set -gx AWS_PROFILE "$aws_profile"
+      aws --no-cli-pager sts get-caller-identity
   end
-  set -gx AWS_PROFILE "$aws_profile"
-  aws --no-cli-pager sts get-caller-identity
 end
 
 function awsprofile
   set aws_profile (aws configure list-profiles | fzf --header 'AWS profile')
-  if ! aws --profile "$aws_profile" sts get-caller-identity > /dev/null 2>&1
-      gimme-aws-creds --profile "$aws_profile"
+  if test -n "$aws_profile"
+      if ! aws --profile "$aws_profile" sts get-caller-identity > /dev/null 2>&1
+          gimme-aws-creds --profile "$aws_profile"
+      end
+      set -gx AWS_PROFILE "$aws_profile"
+      aws --no-cli-pager sts get-caller-identity
   end
-  set -gx AWS_PROFILE "$aws_profile"
-  aws --no-cli-pager sts get-caller-identity
 end
