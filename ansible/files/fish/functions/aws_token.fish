e37892bb99e547d0ef79d122bd9b553d234996e8
Olle Hallin <olle.hallin@hit.se>
2021-10-28 09:28:59 +0200

    Made aws_token.fish accept profile name from command line instead of fzf

--- ansible/files/fish/functions/aws_token.fish
+++ ansible/files/fish/functions/aws_token.fish
@@ -1,5 +1,9 @@
 function aws_token
-  set aws_profile (aws configure list-profiles | fzf --header 'AWS profile')
+  if test -n $argv[1]
+      set aws_profile $argv[1]
+  else
+      set aws_profile (aws configure list-profiles | fzf --header 'AWS profile')
+  end
   if test -n "$aws_profile"
       if ! aws --profile "$aws_profile" sts get-caller-identity > /dev/null 2>&1
           gimme-aws-creds --profile "$aws_profile"
@@ -10,12 +14,5 @@ function aws_token
 end
 
 function awsprofile
-  set aws_profile (aws configure list-profiles | fzf --header 'AWS profile')
-  if test -n "$aws_profile"
-      if ! aws --profile "$aws_profile" sts get-caller-identity > /dev/null 2>&1
-          gimme-aws-creds --profile "$aws_profile"
-      end
-      set -gx AWS_PROFILE "$aws_profile"
-      aws --no-cli-pager sts get-caller-identity
-  end
+    aws_token $argv
 end
