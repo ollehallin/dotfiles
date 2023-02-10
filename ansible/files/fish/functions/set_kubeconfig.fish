db5bc148070b0403e0569bf482e1ff70d6cbb00e
Olle Hallin <olle.hallin@hit.se>
2023-02-10 09:08:09 +0100

    Updated set_kubeconfig.fish to create union $HOME/.kube/config

--- ansible/files/fish/functions/set_kubeconfig.fish
+++ ansible/files/fish/functions/set_kubeconfig.fish
@@ -1,4 +1,4 @@
-function set_kubeconfig --description 'Sets KUBECONFIG to all files in $HOME/.kube/'
+function set_kubeconfig --description 'Sets KUBECONFIG to all files in $HOME/.kube/* and updates $HOME/.kube/config'
   set --local result
   for file in (ls -1 $HOME/.kube/*/*config)
     set result "$file:$result"
@@ -7,6 +7,8 @@ function set_kubeconfig --description 'Sets KUBECONFIG to all files in $HOME/.ku
     set result "$HOME/.kube/config:$result"
   end
 
-  set -Ux KUBECONFIG "$result"
-  echo "KUBECONFIG=$KUBECONFIG"
+  set result (echo $result | sed 's/:$//g')
+
+  env KUBECONFIG="$result" kubectl config view --flatten > $HOME/.kube/config
+  echo "Created $HOME/.kube/config to contain the union of $result"
 end
