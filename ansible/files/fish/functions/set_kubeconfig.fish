f61d0d6f04d1cead5bb813c99d712377faf0487b
Olle Hallin <olle.hallin@hit.se>
2023-11-16 17:04:37 +0100

    set_kubeconfig.fish now only sets KUBECONFIG

--- ansible/files/fish/functions/set_kubeconfig.fish
+++ ansible/files/fish/functions/set_kubeconfig.fish
@@ -1,14 +1,4 @@
-function set_kubeconfig --description 'Sets KUBECONFIG to all files in $HOME/.kube/* and updates $HOME/.kube/config'
-  set --local result
-  for file in (ls -1 $HOME/.kube/*/*config)
-    set result "$file:$result"
-  end
-  if test -f $HOME/.kube/config
-    set result "$HOME/.kube/config:$result"
-  end
-
-  set result (echo $result | sed 's/:$//g')
-
-  env KUBECONFIG="$result" kubectl config view --flatten > $HOME/.kube/config
-  echo "Created $HOME/.kube/config to contain the union of $result"
+function set_kubeconfig --description 'Sets KUBECONFIG to all files in $HOME/.kube/*/*config'
+  set -Ux KUBECONFIG (find $HOME/.kube/ -name '*kubeconfig' | sort | xargs | sed 's/ /:/g')
+  echo "$KUBECONFIG"
 end
